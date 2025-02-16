66326dacc7cf323826a49b2adbb7c6671d7cc9f0
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-02-16 12:30:52 -0700

    create CustomeUser and Subscriptin model

--- subscriptionTracker/tracker/models.py
+++ subscriptionTracker/tracker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    
+    class Meta:
+        swappable = "AUTH_USER_MDDEL"
+    
+    def __str__(self):
+        return self.username
+    
+class Subscription(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='subscriptions')
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    renewal_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.name} - {self.user.username}'
